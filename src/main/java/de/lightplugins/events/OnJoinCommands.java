4dee0adefece8dede45dde829af08240873cd482
lightplugins <lightningdesign@outloo.de>
2024-01-19 22:50:25 +0100

    joinCommand

--- src/main/java/de/lightplugins/events/OnJoinCommands.java
+++ src/main/java/de/lightplugins/events/OnJoinCommands.java
@@ -1,6 +1,7 @@
 package de.lightplugins.events;
 
 import de.lightplugins.master.Ashura;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -18,6 +19,22 @@ public class OnJoinCommands implements Listener {
         Player player = event.getPlayer();
 
         FileConfiguration settings = Ashura.settings.getConfig();
+
+        boolean enable = settings.getBoolean("settings.spawnOnJoin.enable");
+
+        if(!enable) {
+            return;
+        }
+
+        String command = settings.getString("settings.spawnOnJoin.command");
+
+        if(command == null) {
+            return;
+        }
+
+        command = command.replace("#player#", player.getName());
+
+        Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), command);
 
     }
 }
